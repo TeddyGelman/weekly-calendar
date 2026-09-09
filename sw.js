2f0075e32b75b6ebee259cde878855424ebbb8cd
TeddyGelman <teddyjtgelman@gmail.com>
2026-09-10 00:42:36 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,3 @@
-const CACHE='weekly-planner-v12';
-const CORE=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-512.png','https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>Promise.allSettled(CORE.map(u=>c.add(u))))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET') return;
-  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
-});
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',()=>{});
